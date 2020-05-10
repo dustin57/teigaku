51a2b210dfd8882325ff3ed5337b8962fb02a632
Toru Yamaguchi <toru.yamaguchi57@gmail.com>
2020-05-10 21:31:52 +0900

    chenge PDO

--- config.php
+++ config.php
@@ -1,7 +1,12 @@
 <?php
 
-ini_set('display_errors', 1);
-
-const DSN = 'mysql:host=localhost;dbname=subsc';
-const DB_USERNAME = 'dbuser';
-const DB_PASSWORD = 'Avril3641';
+$db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+$db['dbname'] = ltrim($db['path'], '/');
+$dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
+$user = $db['user'];
+$password = $db['pass'];
+$options = array(
+  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+  PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
+);
