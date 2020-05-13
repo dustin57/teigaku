f2ce41a1e4da36b3204713093f9b2d5c0e83528f
Toru Yamaguchi <toru.yamaguchi57@gmail.com>
2020-05-13 23:20:51 +0900

    Update Subscriptions.php

--- Subscriptions.php
+++ Subscriptions.php
@@ -5,12 +5,24 @@ namespace MyApp;
 class Subscriptions {
   private $_db;
 
+
   public function __construct() {
     $this->_createToken();
 
+    $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+    $db['dbname'] = ltrim($db['path'], '/');
+    $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
+    $user = $db['user'];
+    $password = $db['pass'];
+    $options = array(
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+      PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
+    );
+
     try {
-      $this->_db = new \PDO($dsn,$user,$password/*,$options*/);
-      $this->_db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+      $this->_db = new \PDO($dsn, $user, $password, $options);
+      // $this->_db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
     } catch (\PDOException $e) {
       echo $e->getMessage();
       exit;
