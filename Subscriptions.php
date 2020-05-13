de72719efdbfdff13f5b4f1487ca5f0ea1be24c3
Toru Yamaguchi <toru.yamaguchi57@gmail.com>
2020-05-13 22:21:20 +0900

    Update Subscriptions.php

--- Subscriptions.php
+++ Subscriptions.php
@@ -9,26 +9,12 @@ class Subscriptions {
     $this->_createToken();
 
     try {
-      $this->_dbConnect();
+      $this->_db = new \PDO($dsn,$user,$password,$options);
+      // $this->_db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
     } catch (\PDOException $e) {
       echo $e->getMessage();
       exit;
     }
-  }
-
-  private function _dbConnect(){
-    $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
-    $db['dbname'] = ltrim($db['path'], '/');
-    $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
-    $user = $db['user'];
-    $password = $db['pass'];
-    $options = array(
-      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-      PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
-    );
-    $dbh = new PDO($dsn,$user,$password,$options);
-    return $dbh;
   }
 
   private function _createToken() {
