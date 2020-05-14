5cde03d524f5506a959af88c9992d4b52b197316
Toru Yamaguchi <toru.yamaguchi57@gmail.com>
2020-05-14 21:33:21 +0900

    Update Subscriptions.php

--- Subscriptions.php
+++ Subscriptions.php
@@ -12,16 +12,7 @@ class Subscriptions {
 
 
     try {
-      $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
-      $db['dbname'] = ltrim($db['path'], '/');
-      $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
-      $user = $db['user'];
-      $password = $db['pass'];
-      $options = array(
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
-      );
+      
       $this->_db = new \PDO($dsn,$user,$password,$options);
     } catch (\PDOException $e) {
       echo $e->getMessage();
