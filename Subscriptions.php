fc3cbbd93783332a5a0baacdd855cfb19cfcf34f
Toru Yamaguchi <toru.yamaguchi57@gmail.com>
2020-05-14 21:12:37 +0900

    Update Subscriptions.php

--- Subscriptions.php
+++ Subscriptions.php
@@ -9,6 +9,17 @@ class Subscriptions {
   public function __construct() {
     $this->_createToken();
 
+    $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+    $db['dbname'] = ltrim($db['path'], '/');
+    $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
+    $user = $db['user'];
+    $password = $db['pass'];
+    $options = array(
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+      PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
+    );
+    
     try {
       $this->_db = new \PDO($dsn,$user,$password,$options);
     } catch (\PDOException $e) {
