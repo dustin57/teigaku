f244561d85adecb2ef28dbcd65397f0d1d45c1f5
Toru Yamaguchi <toru.yamaguchi57@gmail.com>
2020-05-14 21:28:24 +0900

    Update Subscriptions.php

--- Subscriptions.php
+++ Subscriptions.php
@@ -9,18 +9,19 @@ class Subscriptions {
   public function __construct() {
     $this->_createToken();
 
-    $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
-    $db['dbname'] = ltrim($db['path'], '/');
-    $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
-    $user = $db['user'];
-    $password = $db['pass'];
-    $options = array(
-      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-      PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
-    );
-    
+
+
     try {
+      $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+      $db['dbname'] = ltrim($db['path'], '/');
+      $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
+      $user = $db['user'];
+      $password = $db['pass'];
+      $options = array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
+      );
       $this->_db = new \PDO($dsn,$user,$password,$options);
     } catch (\PDOException $e) {
       echo $e->getMessage();
